38eb8407bd5d32c2d213bc5d65c2137d5a4bd0e8
teranum <tera63614@gmail.com>
2024-11-29 21:59:48 +0900

    update

--- CSharp_test/MainWindow.xaml.cs
+++ CSharp_test/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using LS.XingApi;
+using System.Collections;
+using System.Text;
 using System.Windows;
 
 namespace CSharp_test
@@ -46,13 +48,13 @@ namespace CSharp_test
 
             _api.OnMessageEvent += (s, e) =>
             {
-                AppentResult(e.Message);
+                AppendResult(e.Message);
             };
 
             _api.OnRealtimeEvent += (s, e) =>
             {
                 string text = $"[{e.TrCode}] {e.Key} {e.RealtimeBody}";
-                AppentResult(text);
+                AppendResult(text);
             };
         }
 
@@ -65,7 +67,7 @@ namespace CSharp_test
         [ObservableProperty]
         public string _ResultText;
 
-        void AppentResult(string text)
+        void AppendResult(string text)
         {
             ResultText += text + "\n";
         }
@@ -84,25 +86,85 @@ namespace CSharp_test
                 case "로그인":
                     {
 
-                        AppentResult("로그인 요청중...");
+                        AppendResult("로그인 요청중...");
                         await _api.ConnectAsync(UserId, UserPwd, CertPwd);
-                        AppentResult(_api.LastMessage);
+                        AppendResult(_api.LastMessage);
                     }
                     break;
                 case "로그아웃":
                     {
-                        AppentResult("로그아웃");
+                        AppendResult("로그아웃");
                         _api.Close();
                     }
                     break;
                 case "업종-전체조회":
                     {
-                        var response = await _api.RequestAsync("t8424", "0"); // 0: 전체, 1: 코스피업종, 2: 코스닥업종, 3: 섹터지수, 4: 특수계열지수
-                        AppentResult(_api.LastMessage);
+                        var tr_cd = "t8424";
+                        var response = await _api.RequestAsync(tr_cd, "0"); // 0: 전체, 1: 코스피업종, 2: 코스닥업종, 3: 섹터지수, 4: 특수계열지수
+                        AppendResult($"{tr_cd}: {_api.LastMessage}");
+                        if (response is not null)
+                        {
+                            AppendResult(response);
+                        }
                     }
                     break;
             }
         }
 
+        void AppendResult(ResponseTrData response)
+        {
+            StringBuilder sb = new();
+            if (response is not null)
+            {
+                foreach (var (key, value) in response.body)
+                {
+                    if (value is IList list)
+                    {
+                        sb.AppendLine($"{key}: [{list.Count}]");
+                        foreach (var line in list)
+                        {
+                            if (line is IDictionary dict)
+                            {
+                                sb.Append('{');
+                                bool first = true;
+                                foreach (DictionaryEntry item in dict)
+                                {
+                                    if (!first)
+                                        sb.Append(", ");
+                                    if (item.Value is string str)
+                                        sb.Append($"\"{item.Key}\": \"{str}\"");
+                                    else
+                                        sb.Append($"\"{item.Key}\": {item.Value}");
+                                    first = false;
+                                }
+                                sb.AppendLine("}");
+                            }
+                            else
+                                sb.AppendLine(line.ToString());
+                        }
+                    }
+                    else if (value is IDictionary dict)
+                    {
+                        sb.AppendLine($"{key}: [{dict.Count}]");
+                        sb.Append('{');
+                        bool first = true;
+                        foreach (DictionaryEntry item in dict)
+                        {
+                            if (!first)
+                                sb.Append(", ");
+                            if (item.Value is string str)
+                                sb.Append($"\"{item.Key}\": \"{str}\"");
+                            else
+                                sb.Append($"\"{item.Key}\": {item.Value}");
+                            first = false;
+                        }
+                        sb.AppendLine("}");
+                    }
+                    else 
+                        sb.AppendLine(value.ToString());
+                }
+            }
+            AppendResult(sb.ToString());
+        }
     }
 }
